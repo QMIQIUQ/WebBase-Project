3f383d4e27a6433e0bc89cad47a233c87ff2cc7f
QMIQIUQ <dogaang@gmail.com>
2021-12-27 11:57:08 +0800

    testing

--- Project/resources/views/index.blade.php
+++ Project/resources/views/index.blade.php
@@ -13,8 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
+haha
                 </div>
             </div>
         </div>
